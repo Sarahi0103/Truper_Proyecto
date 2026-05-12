583c586e830ff802294e80406c92e2a731e121f0
Sarahi0103 <karla.gomez.23e@utzmg.edu.mx>
2026-05-12 23:22:14 +0000

    Clean root index wrapper content

--- index.php
+++ index.php
@@ -1,9 +1,2 @@
 <?php
 require __DIR__ . '/public/index.php';
-<head>
-
-    <meta charset="UTF-8">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Truper - Catálogo de Herramientas</title>
