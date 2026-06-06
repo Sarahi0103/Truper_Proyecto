1deb7a258be1a5e1c3646c80bfa78ddee1fecd8d
Karla G <karla.gomez.23e@utzmg.edu.mx>
2026-06-06 14:38:31 -0600

    fix: bump orders.js script version to bypass browser cache

--- public/orders.php
+++ public/orders.php
@@ -379,7 +379,7 @@ $is_admin = (($_SESSION['role'] ?? '') === 'admin');
         window.TRUPER_COMPANY_WHATSAPP = '<?php echo htmlspecialchars(whatsapp_phone_digits(), ENT_QUOTES, 'UTF-8'); ?>';
         window.TRUPER_ORDERS_ROLE = '<?php echo htmlspecialchars($_SESSION['role'] ?? 'client', ENT_QUOTES, 'UTF-8'); ?>';
     </script>
-    <script src="js/orders.js?v=20260605_v8" charset="UTF-8"></script>
+    <script src="js/orders.js?v=20260606_v1" charset="UTF-8"></script>
     <script src="js/barcode-scanner.js"></script>
     <script>
         function logout() {
