123c497288466c778856be6d93d4e790c69180af
Karla G <karla.gomez.23e@utzmg.edu.mx>
2026-06-18 11:45:33 -0600

    Add responsive-complete.css link to cashier.php

--- public/cashier.php
+++ public/cashier.php
@@ -14,6 +14,7 @@ $user_name = htmlspecialchars($_SESSION['name'] ?? 'Usuario', ENT_QUOTES, 'UTF-8
     <link rel="stylesheet" href="css/styles.css?v=4.1">
     <link rel="stylesheet" href="css/theme.css?v=4.1">
     <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="stylesheet" href="css/responsive-complete.css?v=4.1">
     <style>
     /* Cashier specific styles */
     .cashier-container {
